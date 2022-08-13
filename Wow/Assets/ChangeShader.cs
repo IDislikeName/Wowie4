1173b9efe2e263f5f799d498eac635c0d4b0ed40
Xiao <1@xiao.studio>
2022-08-13 15:50:47 -0400

    Merge fix

--- Wow/Assets/ChangeShader.cs
+++ Wow/Assets/ChangeShader.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class ChangeShader : MonoBehaviour
 {
-    public Camera camera;
+    //public Camera camera;
     
     // Start is called before the first frame update
     void Start()
     {
-        camera.SetReplacementShader ();
+        //camera.SetReplacementShader ();
     }
 
     // Update is called once per frame
